e01274c4c2e85bbe2cd240a1ff2908baecf3af6d
fox <l_aptsiauri@cu.edu.ge>
2021-02-05 18:39:45 +0400

    Update Countdown Widget

--- JSInsertions/TrainingCourse.js
+++ JSInsertions/TrainingCourse.js
@@ -31,4 +31,12 @@ if( document.readyState !== 'loading' ) {
             }
         }
     }, 2000);
+    setInterval(function (){
+        let LinkCurrentPKPKPK = window.location.href;
+        if(LinkCurrentPKPKPK === 'https://office2.businessbombshells.team/app.html#/TrainingCourses'){
+            $('.actions').css('display', 'none');
+            $('.c-black .p-r-15 .ng-binding').css('color', '#FFFFFF');
+            $('.c-black .p-r-15 .ng-binding').attr('style', '#ffffff !important');
+        }
+    }, 2000);
 }
