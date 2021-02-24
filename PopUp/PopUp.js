cab17e40bd628e30081a829df92d2934308f596a
fox <l_aptsiauri@cu.edu.ge>
2021-02-24 10:19:32 +0400

    PopUp Update

--- PopUp/PopUp.js
+++ PopUp/PopUp.js
@@ -1,39 +1,15 @@
 $(function (){
-    new Splide( '#splide' ).mount();
 
     var container = document.createElement('div');
-    container.style.position = 'fixed';
-    container.style.top = '0';
-    container.style.left = '0';
-    container.style.width = '100%';
-    container.style.height = '100%';
-    container.style.overflow = 'visible';
-    container.style.zIndex = '9990';
-    container.style.backgroundColor = 'rgb(242, 179, 174, 0.5)';
+    container.setAttribute('id', 'hoverBackgroundContainer');
 
-    var slider = document.createElement('div');
-    slider.setAttribute('id', 'splide');
-    slider.setAttribute('class', 'splide');
+    var containerForSlider = document.createElement('div');
+    containerForSlider.setAttribute('id', 'popupContainerLA');
 
-    this.contain = document.createElement('div');
-    this.contain.setAttribute('class', 'splide__track');
-    this.slider = document.createElement('ul');
-    this.slider.setAttribute('class', 'splide__list');
-    this.slideOne = document.createElement('li');
-    this.slideOne.setAttribute('class', 'splide__slide');
-    this.slideTwo = document.createElement('li');
-    this.slideTwo.setAttribute('class', 'splide__slide');
-    slider.appendChild(this.contain);
-    this.contain.appendChild(this.slider);
-    this.slider.appendChild(this.slideOne);
-    this.slider.appendChild(this.slideTwo);
+    container.appendChild(containerForSlider);
 
-    container.appendChild(slider);
+    containerForSlider.innerHTML = '';
 
-    function sliderJS(){
-        var m = 0;
-        $('.class')
-    }
 
 /*
 <div class="splide__track">
